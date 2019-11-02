fba9a399bfe93b03daed6a7329ed6ddfc9803dc1
khuslentuguldur <khuslen.cyber@gmail.com>
2019-11-02 15:58:14 -0500

    files are modified and deleted. Terraform file is created with the variable.

---
+++ grafana.tf
@@ -0,0 +1,23 @@
+data "template_file" "grafana_values" {
+  template = "${file("./grafana/template_values.yaml")}"
+  vars = {
+    deployment_endpoint = "${lookup(var.dns_endpoint_grafana, "${var.deployment_environment}")}"
+    # deployment_image = "${var.deployment_image}"
+  }
+}
+
+resource "local_file" "grafana_values_local_file" {
+  content  = "${trimspace(data.template_file.grafana_values.rendered)}"
+  filename = "./grafana/.cache/values.yaml"
+}
+
+
+resource "helm_release" "grafana" {
+  name      = "${var.name}"
+  namespace = "${var.deployment_environment}"
+  chart     = "./grafana"
+  version   = "${var.version}"
+  values = [
+    "${data.template_file.grafana_values.rendered}"
+  ]
+}
---
+++ variable.tf
@@ -0,0 +1,26 @@
+variable "deployment_environment" {
+  default = "test"
+}
+
+variable "dns_endpoint_grafana" {
+  type = "map"
+
+  default = {
+    dev  = "dev.grafana.fuchicorp.com"
+    qa   = "qa.grafana.fuchicorp.com"
+    prod = "grafana.fuchicorp.com"
+  }
+}
+variable "version" {
+  default = "6.0.1"
+}
+
+########### Put datasource or Prometheus endpoint as below ##############
+
+variable "datasource_dns_endpoint" {
+  default = "https://test-prometheus.fuchicorp.com"
+}
+
+variable "name" {
+  default = "grafana"
+}
