532f3a4d22e3c676300579ec772cc118002f10b2
khuslentuguldur <khuslen.cyber@gmail.com>
2019-11-05 15:26:18 -0600

    updated

--- variables.tf
+++ variables.tf
@@ -41,60 +41,60 @@ variable "grafana_password" {}
 ############### Grafana Section Ends Right Here ################
 ###############                             ######################
 
-variable "namespace" {
-  default = "tools"
-}
-
-variable "vault_token" {
-  default = "Redhat"
-}
-
-variable "git_token" {
-  default = "this-is-git-token"
-}
-
-variable "grafana_service_port" {
-  default     = 8080
-  description = "Please do not change this ports."
-}
-
-variable "vault_service_port" {
-  default     = 8082
-  description = "Please do not change this ports."
-}
-
-variable "nexus_service_port" {
-  default     = 8083
-  description = "Please do not change this ports."
-}
-
-variable "tiller_version" {
-  default     = "v2.11.0"
-  description = "Please provide version of the tiller."
-}
-
-variable "tiller_namespace" {
-  default     = "kube-system"
-  description = "Tiller by default will deploy to kube-system"
-}
-
-variable "repo_port" {
-  default = 8085
-}
-
-variable "email" {
-  default = "fuchicorpsolutions@gmail.com"
-}
-
-variable "project" {
-  default = "fuchicorp-project-256020"
-}
-
-variable "jenkins" {
-  type = "map"
-}
-
-# Github Integration for Jenkins Master
-variable "jenkins_auth_client_id" {}
-
-variable "jenkins_auth_secret" {}
+# variable "namespace" {
+#   default = "tools"
+# }
+#
+# variable "vault_token" {
+#   default = "Redhat"
+# }
+#
+# variable "git_token" {
+#   default = "this-is-git-token"
+# }
+#
+# variable "grafana_service_port" {
+#   default     = 8080
+#   description = "Please do not change this ports."
+# }
+#
+# variable "vault_service_port" {
+#   default     = 8082
+#   description = "Please do not change this ports."
+# }
+#
+# variable "nexus_service_port" {
+#   default     = 8083
+#   description = "Please do not change this ports."
+# }
+#
+# variable "tiller_version" {
+#   default     = "v2.11.0"
+#   description = "Please provide version of the tiller."
+# }
+#
+# variable "tiller_namespace" {
+#   default     = "kube-system"
+#   description = "Tiller by default will deploy to kube-system"
+# }
+#
+# variable "repo_port" {
+#   default = 8085
+# }
+#
+# variable "email" {
+#   default = "fuchicorpsolutions@gmail.com"
+# }
+#
+# variable "project" {
+#   default = "fuchicorp-project-256020"
+# }
+#
+# variable "jenkins" {
+#   type = "map"
+# }
+#
+# # Github Integration for Jenkins Master
+# variable "jenkins_auth_client_id" {}
+#
+# variable "jenkins_auth_secret" {}
