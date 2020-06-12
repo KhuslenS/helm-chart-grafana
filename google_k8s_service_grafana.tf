7852081d7911e6b7afcfbef3dda23d3a03586498
khuslens <khuslen.cyber@gmail.com>
2020-06-11 19:51:03 -0500

    updated

--- google_k8s_service_grafana.tf
+++ google_k8s_service_grafana.tf
@@ -2,7 +2,6 @@ data "template_file" "template_values" {
   template = "${file("./helm-grafana/template_values.yaml")}"
 
   vars = {
-    deployment_endpoint     = "${lookup(var.grafana-dns_endpoint_grafana, "${var.grafana-deployment_environment}")}"
     datasource_dns_endpoint = "${var.grafana-datasource_dns_endpoint}"
     grafana_password        = "${var.grafana_password}"
     grafana_username        = "${var.grafana_username}"
